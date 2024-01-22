bf00e089d73a04f394b5fde129b0ee2a9ceaa7c2
TStark22 <astarkoff77@gmail.com>
2024-01-22 13:18:55 +0300

    22.01.23 Ready!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanH1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanH2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat beanC1 = (Cat) applicationContext.getBean("cat");
+        Cat beanC2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Равны-ли бины 'Hello world': " + beanH1.equals(beanH2));
+        System.out.println("Равны-ли бины Cat: " + beanC1.equals(beanC2));
     }
 }
